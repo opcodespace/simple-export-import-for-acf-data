4a5beeafc6d69409cdcb5cce79fad0ea9613abb2
Md. Rayhan Uddin <rayhanwily@gmail.com>
2021-07-18 14:17:00 +0600

    back

--- view/export_import.php
+++ view/export_import.php
@@ -1,12 +1,3 @@
-<style>
-    .submin_btn_wrapper {
-        text-align: center;
-        margin-top: 20px;
-    }
-    select{
-        min-width: 190px;
-    }
-</style>
 <div class="wrap">
     <h1>Simple Export Import</h1>
     <div class="card">
@@ -15,81 +6,61 @@
             <form action="<?= admin_url('/admin-post.php') ?>" method="post">
                 <?php    wp_nonce_field('seip_export'); ?>
                 <input type="hidden" name="action" value="seip_export">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td><label for="">Type</label></td>
-                            <td>: <select name="post_type">
-                                <option value="">Please Select Type</option>
-                                <?php foreach(get_post_types([], 'objects') as $post_type):
-                                    echo "<option value='{$post_type->name}'>{$post_type->label}</option>";
-                                endforeach;
-                                ?>
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="">Post/Page</label></td>
-                        <td>: <select name="post_id" id="">
+                <p>
+                    <label for="">Type</label>
+                    <select name="post_type">
+                        <option value="">Please Select Type</option>
+                        <?php foreach(get_post_types([], 'objects') as $post_type):
+                        echo "<option value='{$post_type->name}'>{$post_type->label}</option>";
+                        endforeach;
+                        ?>
+                    </select>
+                </p>
+                <p>
+                    <label for="">Post/Page</label>
+                    <select name="post_id" id="">
 
-                        </select>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="submin_btn_wrapper">
-            <input type="submit" class="button button-primary" value="Export">
+                    </select>
+                </p>
+                <div>
+                    <input type="submit" class="button button-primary" value="Export">
+                </div>
+            </form>
         </div>
-    </form>
-</div>
-</div>
+    </div>
 
-<div class="card">
-    <h2>Import Page/Post</h2>
-    <div class="import-form-wrapper">
-        <form action="<?= admin_url('/admin-post.php') ?>" method="post" enctype="multipart/form-data">
-            <?php    wp_nonce_field('seip_import'); ?>
-            <input type="hidden" name="action" value="seip_import">
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Override existing page/post </td>
-                        <td>: 
-                            <input type="checkbox">
-                            <label for="">Yes</label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="">Type</label>
-                        </td>
-                        <td>: <select name="post_type">
-                            <option value="">Please Select Type</option>
-                            <?php foreach(get_post_types([], 'objects') as $post_type):
-                                echo "<option value='{$post_type->name}'>{$post_type->label}</option>";
-                            endforeach;
-                            ?>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td><label for="">Post/Page</label></td>
-                    <td>: <select name="post_id" id="">
+    <div class="card">
+        <h2>Import Page/Post</h2>
+        <div class="import-form-wrapper">
+            <form action="<?= admin_url('/admin-post.php') ?>" method="post" enctype="multipart/form-data">
+                <?php    wp_nonce_field('seip_import'); ?>
+                <input type="hidden" name="action" value="seip_import">
+                <p>
+                    <label for="">Type</label>
+                    <select name="post_type">
+                        <option value="">Please Select Type</option>
+                        <?php foreach(get_post_types([], 'objects') as $post_type):
+                            echo "<option value='{$post_type->name}'>{$post_type->label}</option>";
+                        endforeach;
+                        ?>
+                    </select>
+                </p>
+                <p>
+                    <label for="">Post/Page</label>
+                    <select name="post_id" id="">
 
-                    </select></td>
-                </tr>
-                <tr>
-                    <td><label for="file">Upload File</label></td>
-                    <td>: <input type="file" name="file" id="file"></td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="submin_btn_wrapper">
-            <input type="submit" class="button button-primary" value="Export">
+                    </select>
+                </p>
+                <p>
+                    <label for="file">Upload File</label>
+                    <input type="file" name="file" id="file">
+                </p>
+                <div>
+                    <input type="submit" class="button button-primary" value="Export">
+                </div>
+            </form>
         </div>
-    </form>
-</div>
-</div>
+    </div>
 </div>
 
 <script>
@@ -100,8 +71,8 @@
               method: "POST",
               url: "<?=admin_url('/admin-ajax.php'); ?>",
               data: { action: "seip_get_all_posts", post_type: $(this).val(), _wpnonce: $('#_wpnonce').val()}
-          })
-            .done(function( response ) {
+            })
+              .done(function( response ) {
                 if(response.success){
                     let options = '';
 
@@ -111,7 +82,7 @@
 
                     _this_parent.find('[name="post_id"]').html(options);
                 }
-            }, _this_parent);
+              }, _this_parent);
         })
     })
 </script>
