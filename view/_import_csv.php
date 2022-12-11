8ebd7115ef44dba458f2984345e85393fb2dde2b
Md. Rayhan Uddin <rayhanwily@gmail.com>
2022-12-11 13:17:27 +0600

    Update Design

--- view/_import_csv.php
+++ view/_import_csv.php
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
                                 <tr class="">
                                     <td><label class="label_block" for="file">Type</label></td>
                                     <td>
-                                     <select name="post_type" class="chosen-select">
+                                       <select name="post_type" class="chosen-select">
                                         <option value="">Please Select Type</option>
                                         <?php foreach (get_post_types([], 'objects') as $post_type):
                                             ?>
@@ -34,10 +34,10 @@ if (!defined('ABSPATH')) {
                                     </select>
                                 </td>
                             </tr>
-                                <tr class="">
-                                    <td><label class="label_block" for="file">Description</label></td>
-                                    <td>
-                                     <textarea name="" id="" cols="32" rows="5" style="border:1px solid #c3c4c7 !important"></textarea>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <input type="submit" class="button button-primary" value="Import">
                                 </td>
                             </tr>
                         </tbody>
@@ -47,92 +47,102 @@ if (!defined('ABSPATH')) {
         </div>
     </div> 
     <div class="card" style="max-width: 100%;">
-    <h2>Maping</h2>
-    <div class="import-form-wrapper">
-        <form action="<?php echo esc_url(admin_url('/admin-post.php')) ?>" method="post" enctype="multipart/form-data">
-            <?php wp_nonce_field('seip_import'); ?>
-            <input type="hidden" name="action" value="seip_import">
-            <div class="block_csv_box">
-                <table>
-                    <tbody>
-                        <tr class="maping_row">
-                            <td>
-                                <div class="maping_block_wrapper">
-                                    <div class="item_number">1.</div>
-                                    <div class="csv_ttl">Post Image</div>
-                                    <div class="csv_meta_key"><b>Meta Key : </b>Image</div>
-                                    <div class="csv_type"><b>Type : </b>Image</div>
-                                </div>
-                            </td>
-                            <td>
-                                <select name="post_type" class="chosen-select">
-                                    <option value="">Please Select Type</option>
-                                    <?php foreach (get_post_types([], 'objects') as $post_type):
+        <h2>Maping</h2>
+        <div class="import-form-wrapper">
+            <form action="<?php echo esc_url(admin_url('/admin-post.php')) ?>" method="post" enctype="multipart/form-data">
+                <?php wp_nonce_field('seip_import'); ?>
+                <input type="hidden" name="action" value="seip_import">
+                <div class="block_csv_box">
+                    <table>
+                        <tbody>
+                            <tr class="maping_row">
+                                <td>
+                                    <div class="maping_block_wrapper">
+                                        <div class="item_number">1.</div>
+                                        <div class="csv_ttl">Post Image</div>
+                                        <div class="csv_meta_key"><b>Meta Key : </b>Image</div>
+                                        <div class="csv_type"><b>Type : </b>Image</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <select name="post_type" class="chosen-select">
+                                        <option value="">Please Select Type</option>
+                                        <?php foreach (get_post_types([], 'objects') as $post_type):
+                                            ?>
+                                            <option value='<?php echo esc_attr($post_type->name) ?>'><?php echo esc_attr($post_type->label) ?></option>
+                                            <?php
+                                        endforeach;
                                         ?>
-                                        <option value='<?php echo esc_attr($post_type->name) ?>'><?php echo esc_attr($post_type->label) ?></option>
-                                        <?php
-                                    endforeach;
-                                    ?>
-                                </select>
+                                    </select>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, fuga. Facere placeat ratione rem expedita cum inventore. Culpa impedit omnis ducimus ab explicabo reiciendis, voluptates ipsam minima in, architecto consectetur.</p>
+                                </td>
+                            </tr>
+                            <tr class="maping_row">
+                                <td>
+                                    <div class="maping_block_wrapper">
+                                        <div class="item_number">2.</div>
+                                        <div class="csv_ttl">Gallery</div>
+                                        <div class="csv_meta_key"><b>Meta Key : </b>Gallery</div>
+                                        <div class="csv_type"><b>Type : </b>Gallery</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div id="export_mulit_pages"
+                                    multiple
+                                    placeholder="Select page/post"
+                                    name="post_ids"
+                                    autofocus
+                                    >
+                                </div>
                             </td>
                         </tr>
                         <tr class="maping_row">
                             <td>
                                 <div class="maping_block_wrapper">
-                                    <div class="item_number">2.</div>
-                                    <div class="csv_ttl">Gallery</div>
-                                    <div class="csv_meta_key"><b>Meta Key : </b>Gallery</div>
-                                    <div class="csv_type"><b>Type : </b>Gallery</div>
+                                    <div class="item_number">3.</div>
+                                    <div class="csv_ttl">Object</div>
+                                    <div class="csv_meta_key"><b>Meta Key : </b>Object</div>
+                                    <div class="csv_type"><b>Type : </b>Object</div>
                                 </div>
                             </td>
                             <td>
-                                <div id="export_mulit_pages"
-                                multiple
-                                placeholder="Select page/post"
-                                name="post_ids"
-                                autofocus
-                                >
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <div class="seip_td_second_label">Label 1</div>
+                                            <div id="export_mulit_pages"
+                                            multiple
+                                            placeholder="Select page/post"
+                                            name="post_ids"
+                                            autofocus
+                                            >
+                                        </td>
+                                        <td>
+                                            <div class="seip_td_second_label">Label 2</div>
+                                            <div id="export_mulit_pages"
+                                            multiple
+                                            placeholder="Select page/post"
+                                            name="post_ids"
+                                            autofocus
+                                            >
+                                        </div></td>
+                                    </tr>
+                                </table>                                
                             </div>
                         </td>
                     </tr>
-                    <tr class="maping_row">
-                        <td>
-                            <div class="maping_block_wrapper">
-                                <div class="item_number">3.</div>
-                                <div class="csv_ttl">Object</div>
-                                <div class="csv_meta_key"><b>Meta Key : </b>Object</div>
-                                <div class="csv_type"><b>Type : </b>Object</div>
-                            </div>
-                        </td>
-                        <td>
-                            <div style="margin-bottom: 10px;" id="export_mulit_pages"
-                            multiple
-                            placeholder="Select page/post"
-                            name="post_ids"
-                            autofocus
-                            >
-                        </div>
-                        <div id="export_mulit_pages"
-                        multiple
-                        placeholder="Select page/post"
-                        name="post_ids"
-                        autofocus
-                        >
-                    </div>
+                </tbody>
+            </table>
+        </div>
+        <table>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" class="button button-primary" value="Import">
                 </td>
             </tr>
-        </tbody>
-    </table>
-</div>
-<table>
-    <tr>
-        <td></td>
-        <td>
-            <input type="submit" class="button button-primary" value="Import">
-        </td>
-    </tr>
-</table>
-</form>
+        </table>
+    </form>
 </div>
 </div>
 
